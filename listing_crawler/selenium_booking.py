6cad092cbedbee153bc5bc205c8568b44c51ef6a
Al <mawaskow@yahoo.com>
2025-09-28 20:58:50 +0100

    successfully scrapes most results
    navigates the continuous scrolling/loading and click button to load more results to get urls of all search results.
    for some reason, the total number of listings is different from the total number of results is different from the actual results. (Galway County: 1022>946>820, Galway City: 429>376>344, Connemara: 431>422>400)

--- listing_crawler/selenium_booking.py
+++ listing_crawler/selenium_booking.py
@@ -11,79 +11,65 @@ Once 75 listings are present, a button appears, prompting to load more.
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
+
+cwd = os.getcwd()
+output_dir = cwd+"/scraping_outputs"
 
 def main():
+    st = time.time()
     urls = {
-        "Galway County":"https://www.booking.com/region/ie/county-galway.en-gb.html", #1022 #946
-        "Galway City": "https://www.booking.com/city/ie/galway.en-gb.html", #429 #376
-        "Connemara": "https://www.booking.com/region/ie/connemara.en-gb.html" #431 #422
+        "Galway County":"https://www.booking.com/region/ie/county-galway.en-gb.html", #1022 #946 #820
+        "Galway City": "https://www.booking.com/city/ie/galway.en-gb.html", #429 #376 #344
+        "Connemara": "https://www.booking.com/region/ie/connemara.en-gb.html" #431 #422 #400
     }
     driver = webdriver.Chrome()
-    driver.get(urls["Galway County"])
-    '''
-    im_flexible_button = driver.find_element(By.XPATH, "//button[@id='flexible-searchboxdatepicker-tab-trigger']")
-    im_flexible_button.click()
-    time.sleep(2)
-    stay_period_selectors = driver.find_elements(By.XPATH, '//input[@name="flexible-los"]')
-    print(stay_period_selectors)
-    for selector in stay_period_selectors:
-        print(selector.get_attribute("value"))
-    stay_period = {
-        "weekend":"value=5_1", # WEEKEND value 5_1
-        "week":"value=1_5",
-        "month":"value=6_28"
-    }
-    stay_month_ul = "id=':r31:'"
-    '''
+    driver.get(urls["Connemara"])
+    #
     see_all_options = driver.find_element(By.XPATH, "//a[@class='de576f5064 b46cd7aad7 ced67027e5 c7a901b0e7 e4f9ca4b0c ca8e0b9533 a9d40b8d51']")
     print(see_all_options.get_attribute("href"))
     see_all_options.click()
     # next page, search results
-    time.sleep(1)
     # get number of results on page
+    time.sleep(1)
     result_heading = driver.find_element(By.XPATH, "//h1[@aria-live='assertive']")
     r_head_txt = result_heading.text
-    print(r_head_txt.split(":")[-1])
-    print(r_head_txt.split(":")[-1].split(" "))
-    tot_res_num = r_head_txt.split(":")[-1].split(" ")[0]
+    # CONTROL
+    tot_res_num = int(r_head_txt.split(":")[-1].split(" ")[1])
+    #tot_res_num = 100
     print("\n", tot_res_num, "total results")
     # scroll down page to dynamically load content
     keep_scrolling = True
     current_n_res = [0]
+    printed = 0
     while keep_scrolling:
-        print(current_n_res)
         current_lsts = driver.find_elements(By.XPATH, "//a[@data-testid='title-link']")
-        print("\n",len(current_lsts))
-        if len(current_lsts) == current_n_res[-1]:
-            keep_scrolling = False
+        if len(current_lsts) != printed:
+            print("\n",len(current_lsts),"listings found")
+            printed = len(current_lsts)
         current_n_res.append(len(current_lsts))
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(1)
-    print("\nExited scrolling while loop")
-    # should have 75 results before clicking to next page
-    # seems like as you scroll, they dynamically load in sets of 25ish?
-    ok = True
-    while ok:
-        try:
-            #load_more_options_btn = driver.find_element(By.XPATH, "//button[@class='de576f5064 b46cd7aad7 d0a01e3d83 dda427e6b5 bbf83acb81 a0ddd706cc']")
-            #driver.execute_script("arguments[0].scrollIntoView();", load_more_options_btn)
-            current_lsts = driver.find_elements(By.XPATH, "//a[@data-testid='title-link']")
-            load_more_options_btn.click()
-            print("\n",len(current_lsts))
-        except:
-            ok = False
-    print("\nButton click loop exited")
-    current_lsts = driver.find_elements(By.XPATH, "//a[@data-testid='title-link']")
-    print(len(current_lsts))
-    load_more_options_btn = driver.find_element(By.XPATH, "//button[@class='de576f5064 b46cd7aad7 d0a01e3d83 dda427e6b5 bbf83acb81 a0ddd706cc']")
-    while load_more_options_btn:
+        if current_n_res[-1] == current_n_res[-2]:
+            try:
+                load_more_options_btn = driver.find_element(By.XPATH, "//button[@class='de576f5064 b46cd7aad7 d0a01e3d83 dda427e6b5 bbf83acb81 a0ddd706cc']")
+                load_more_options_btn.click()
+                time.sleep(1)
+                current_lsts = driver.find_elements(By.XPATH, "//a[@data-testid='title-link']")
+            except Exception as e:
+                print(e)
+        keep_scrolling = len(current_lsts)<tot_res_num
+        if len(current_n_res)>3:
+            if current_n_res[-1] == (current_n_res[-2]+current_n_res[-3])/2:
+                keep_scrolling = False
+    with open(output_dir+"/booking_connemara_urls.txt", "w") as f:
         current_lsts = driver.find_elements(By.XPATH, "//a[@data-testid='title-link']")
-        print(len(current_lsts))
-        time.sleep(1)
-        load_more_options_btn = driver.find_element(By.XPATH, "//button[@class='de576f5064 b46cd7aad7 d0a01e3d83 dda427e6b5 bbf83acb81 a0ddd706cc']")
-        load_more_options_btn.click()
-    time.sleep(1)
+        for element in current_lsts:
+            f.write(element.get_attribute('href')+"\n")
     driver.quit()
+    elapsed = time.time()-st
+    print(f"Found {len(current_lsts)} of {tot_res_num} listings")
+    print(round(elapsed/60,2), "min")
 
 if __name__ == "__main__":
     main()
